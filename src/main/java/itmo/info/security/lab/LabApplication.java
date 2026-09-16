a2222c5d5d1793df95c7ae83cff4c520ecf8c1f1
SolidSn4ke <artsuper472@gmail.com>
2026-09-16 14:03:04 +0300

    init commit

---
+++ src/main/java/itmo/info/security/lab/LabApplication.java
@@ -0,0 +1,13 @@
+package itmo.info.security.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabApplication.class, args);
+	}
+
+}
